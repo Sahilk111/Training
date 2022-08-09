a49b7726d6f772e0e4ee3f9eccf499ebf6bcb818
Sahilk111 <sahil@neoito.com>
2022-08-09 17:24:29 +0530

    Function-2

---
+++ Task-13/app.js
@@ -0,0 +1,13 @@
+// function min(a,b) which returns the least of two numbers a and b.//
+
+function min(a,b) {
+
+    if(a>b){
+        return b;
+    }else{
+        return a;
+    }
+    
+}
+
+min(2,3);
