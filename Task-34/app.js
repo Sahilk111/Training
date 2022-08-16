8e7e397609a970fea0de77aae292ce9c430ab0ce
Sahilk111 <sahil@neoito.com>
2022-08-16 15:39:16 +0530

    Camelize

---
+++ Task-34/app.js
@@ -0,0 +1,15 @@
+// Camelise //
+
+function camelise(str) {
+
+    return str.split('-')
+
+    .map((word,index)=> index==0? word: word[0].toUpperCase() + word.slice(1)      
+        
+    )
+        
+   .join('');
+ 
+}
+
+alert(camelise("demo-project"));
