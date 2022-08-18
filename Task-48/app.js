6e02c6832caf9a2c3b90273d3dbe53e9bbdbc149
Sahilk111 <sahil@neoito.com>
2022-08-18 14:21:24 +0530

    Count Properties

---
+++ Task-48/app.js
@@ -0,0 +1,15 @@
+// Count properties //
+
+function count(salary){
+
+    return Object.keys(salaries).length;
+}
+
+
+let salaries = {
+"John": 100,
+"Pete": 300,
+"Mary": 250
+};
+
+alert(count(salaries));
